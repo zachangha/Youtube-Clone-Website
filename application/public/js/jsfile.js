59ca4bc382a9699516f9c0fd479be7b4e8e31ed5
zachangha <zsangha@live.com>
2023-03-29 13:28:08 -0700

    finished javascript for username and added password character minimum

--- application/public/js/jsfile.js
+++ application/public/js/jsfile.js
@@ -1,7 +1,9 @@
 document.getElementById("username").addEventListener("input", function(ev) {
     let userIput = ev.currentTarget;
     let username = userIput.value;
-    if (username.length >= 3){
+    var firstCharacter = username.charAt(0);
+    var alphanumeric = username.match(/[a-zA-Z0-9]/g);
+    if (alphanumeric.length >= 3 && firstCharacter.match(/[a-zA-Z]/)){
         userIput.classList.add("valid_text")
         userIput.classList.remove("invalid_text")
     }
@@ -9,6 +11,19 @@ document.getElementById("username").addEventListener("input", function(ev) {
         userIput.classList.add("invalid_text")
         userIput.classList.remove("valid_text")
     }
+});
+
+document.getElementById("password").addEventListener("input", function(ev){
+    let userIput = ev.currentTarget;
+    let password = userIput.value;
+    if (password.length >= 8 ){
+        userIput.classList.add("valid_text")
+        userIput.classList.remove("invalid_text")
+    }
+    else{
+        userIput.classList.add("invalid_text")
+        userIput.classList.remove("valid_text")
+    } 
 });
 
 document.getElementsByTagName('form').addEventListener("submit", function(ev){
