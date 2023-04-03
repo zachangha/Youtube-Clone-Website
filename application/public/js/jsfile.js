86ec1186f04c84a459e74fe43c3d68b8a4654bf7
zachangha <zsangha@live.com>
2023-04-03 12:00:58 -0700

    displayed password and username requirements

--- application/public/js/jsfile.js
+++ application/public/js/jsfile.js
@@ -13,6 +13,20 @@ document.getElementById("username").addEventListener("input", function(ev) {
     }
 });
 
+document.getElementById("username").addEventListener("click", function(ev) {
+    var usernameRequirements = document.getElementById("username_requirements")
+    usernameRequirements.style.display = "block";
+});
+
+document.getElementById("password").addEventListener("click", function(ev){
+    var passwordRequirements = document.getElementById("password_requirements")
+    passwordRequirements.style.display = "block";
+})
+
+document.getElementById("email").addEventListener("input", function(ev){
+    
+})
+
 document.getElementById("password").addEventListener("input", function(ev){
     let userIput = ev.currentTarget;
     let password = userIput.value;
@@ -28,8 +42,8 @@ document.getElementById("password").addEventListener("input", function(ev){
 
 document.getElementById("confirm_password").addEventListener("input", function(ev){
     let userIput = ev.currentTarget;
-    var confirm_password = userIput.value;
-    var password = document.getElementById("password").value;
+    let confirm_password = userIput.value;
+    let password = document.getElementById("password").value;
     if (confirm_password == password){
         userIput.classList.add("valid_text")
         userIput.classList.remove("invalid_text")
