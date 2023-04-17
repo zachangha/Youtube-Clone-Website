f94d5039b0711b22eedc27b4721feb45f19c0cbe
zachangha <zsangha@live.com>
2023-04-17 16:27:23 -0700

    created handle bar files and moved header

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Zacharia S Angha" });
 });
 
+router.get("/login", function(req,res){
+  res.render('login');
+});
+router.get("/registration", function(req,res){
+  res.render('registration');
+});
+
+router.get("/postvideo", function(req,res){
+  res.render('postvideo');
+});
+
+router.get("/profile", function(req,res){
+  res.render('profile');
+});
+
+router.get("/viewpost", function(req,res){
+  res.render('viewpost');
+});
+
 module.exports = router;
