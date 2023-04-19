3641250f59892eab286782d8a4568af5cbb4c995
zachangha <zsangha@live.com>
2023-04-18 17:55:27 -0700

    make it where js files will only load on pages that they are needed

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,16 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Zacharia S Angha" });
+  res.render('index', { title: 'CSC 317 App', name:"Zacharia S Angha", js:["index.js"] });
 });
-
-
 
 router.get("/login", function(req,res){
   res.render('login');
 });
 router.get("/registration", function(req,res){
-  res.render('registration');
+  res.render('registration', {title: 'Registration', js:["registration.js"]});
 });
 
 router.get("/postvideo", function(req,res){
