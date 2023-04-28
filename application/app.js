62431e1942f0c929ffc4275751c7c7f07176e301
zachangha <zsangha@live.com>
2023-04-27 23:47:21 -0700

    create database.js and .emv files

--- application/app.js
+++ application/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const createError = require("http-errors");
 const express = require("express");
 const favicon = require('serve-favicon');
--- application/routes/users.js
+++ application/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var db = require('../conf/database');
+/* GET localhost:3000.users */
+router.get('/', async function(req, res, next) {
+  try{
+    let [rows, fields] = await db.query(`select * from users;`);
+    res.status(200).json({rows, fields});
+  }catch(error){
+    next(error);
+  }
 });
 
 module.exports = router;
