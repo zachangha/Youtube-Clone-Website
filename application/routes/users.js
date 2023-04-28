e9253db7cc9e4b4e0db12f405713184856a93832
zachangha <zsangha@live.com>
2023-04-28 14:43:49 -0700

    save registration information on mysql

--- application/routes/users.js
+++ application/routes/users.js
@@ -2,13 +2,30 @@ var express = require('express');
 var router = express.Router();
 var db = require('../conf/database');
 /* GET localhost:3000.users */
-router.get('/', async function(req, res, next) {
+router.post('/registration', async function(req, res, next) {
+  var {username, email, password} = req.body;
   try{
-    let [rows, fields] = await db.query(`select * from users;`);
-    res.status(200).json({rows, fields});
+    var [rows, fields] = await db.execute(`select id from users where username=?`, [username]);
+    if(rows && rows.length > 0)
+    {
+      return res.redirect('/register');
+    }
+    var [rows, fields] = await db.execute(`select id from users where email=?`, [email]);
+    if(rows && rows.length > 0)
+    {
+      return res.redirect('/register');
+    }
+
+    var [resultObject, fields] = await db.execute( `INSERT INTO users (username, email, password) value (?,?,?)`, [username, email, password]);
+    if(resultObject && resultObject.affectedRows == 1){
+      res.redirect('/login');
+    }else{
+      return res.redirect("/register");
+    }
   }catch(error){
     next(error);
   }
+
 });
 
 module.exports = router;
