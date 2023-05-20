d16567e50e04ac62c49ac9c08573e08303cc020a
zachangha <zsangha@live.com>
2023-05-19 23:04:40 -0700

    begin adding comments

--- application/routes/comments.js
+++ application/routes/comments.js
@@ -1,9 +1,28 @@
 var express = require('express');
 const { isLoggedIn } = require('../middleware/auth');
 var router = express.Router();
+var db = require('../conf/database');
 
-router.post('/create', isLoggedIn, function(req, res, next){
-    res.status(201).json(req.body);
+router.post('/create', isLoggedIn, async function(req, res, next){
+    var {userID, username} = req.session.user;
+    var {postId, comment} = req.body;
+
+    try {
+        var [insertResult, _] = await db.execute(
+            `INSERT INTO comments (text, fk_postId, fk_authorId) VALUE (?,?,?)`, [comment, postId, userID]
+        )
+        if (insertResult && insertResult.affectedRows == 1) {
+            return res.status(201).json({
+                commentId: insertResult.insertId,
+                username: username,
+                commentText: comment,
+            });
+        } else {
+            
+        }
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
